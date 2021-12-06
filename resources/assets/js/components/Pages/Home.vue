6a81871a8abe0574ac3bf0ff9885c653f5a9ddb6
hongcui <hong1.cui@gmail.com>
2021-12-06 13:48:06 -0700

    text editing

--- resources/assets/js/components/Pages/Home.vue
+++ resources/assets/js/components/Pages/Home.vue
@@ -3368,7 +3368,7 @@ export default {
 
         if (app.standardCharacters.find(each => each.name == tempWholeCharacter)) {
           app.alreadyExistingCharacter = true;
-          app.alreadyExistingCharacterNotifyMessage = tempWholeCharacter + ' is already exists. Select the existing character in the Search/Create Character box.'
+          app.alreadyExistingCharacterNotifyMessage = tempWholeCharacter + ' already exists. Select the existing character in the Search/Create Character box.'
           // app.firstCharacter = '';
           // app.middleCharacter = '';
           // app.lastCharacter = '';
