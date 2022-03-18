e3887490916dad89b42d948a928aee16cc62d324
cooldev0103 <cool.dev0103@gmail.com>
2022-03-17 22:13:04 -0400

    fixing minor issues

--- app/Http/Controllers/Api/V1/HomeController.php
+++ app/Http/Controllers/Api/V1/HomeController.php
@@ -3255,7 +3255,7 @@ class HomeController extends Controller
                 'owner_name' => $username . '_ver_' . $matrixName,
                 'standard_tag' => $eachCharacter['standard_tag'],
                 'summary' => $eachCharacter['summary'],
-                'usage_count' => 0,
+                'usage_count' => $eachCharacter['summary'],
                 'order' => $eachCharacter['order'],
                 'show_flag' => $eachCharacter['show_flag'],
                 'created_at' => date("Y-m-d") . " " . date("H:i:s"),
