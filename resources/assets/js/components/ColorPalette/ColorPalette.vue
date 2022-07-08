da474c4be8c9730b6714b084ed1750817d6b1824
kajaldhiman <kajal.dhiman@luminoguru.com>
2022-07-08 15:35:35 +0530

    apply tooltip on different color palletes

--- resources/assets/js/components/ColorPalette/ColorPalette.vue
+++ resources/assets/js/components/ColorPalette/ColorPalette.vue
@@ -20,13 +20,13 @@
                 </div>
             </div>
             <div class="pallet-basic-color">
-              <div class="boxes"></div>
-              <div class="boxes"></div>
-              <div class="boxes"></div>
-              <div class="boxes"></div>
-              <div class="boxes"></div>
-              <div class="boxes"></div>
-              <div class="boxes"></div> 
+              <div class="boxes" v-tooltip='"RGB(0, 0, 255)"'></div>
+              <div class="boxes" v-tooltip='"RGB(0, 255, 255)"'></div>
+              <div class="boxes" v-tooltip='"RGB(0, 255, 0)"'></div>
+              <div class="boxes" v-tooltip='"RGB(255, 255, 0)"'></div>
+              <div class="boxes" v-tooltip='"RGB(255, 0, 0)"'></div>
+              <div class="boxes" v-tooltip='"RGB(255, 0, 255)"'></div>
+              <div class="boxes" v-tooltip='"RGB(255, 255, 255)"'></div> 
             </div>        
           </div>
 
@@ -47,26 +47,26 @@
             </div>
             <div class="pallet-color-control">
               <div class="pallet-color-light">
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>       
-                <div class="boxes"></div>        
-                <div class="boxes"></div>
+                <div class="boxes" v-tooltip="'RGB(236, 242, 243)'"></div>
+                <div class="boxes" v-tooltip="'RGB(222, 238, 236)'"></div>
+                <div class="boxes" v-tooltip="'RGB(213, 229, 203)'"></div>
+                <div class="boxes" v-tooltip="'RGB(243, 238, 171)'"></div>
+                <div class="boxes" v-tooltip="'RGB(239, 202, 164)'"></div>
+                <div class="boxes" v-tooltip="'RGB(243, 223, 205)'"></div>
+                <div class="boxes" v-tooltip="'RGB(240, 247, 252)'"></div>       
+                <div class="boxes" v-tooltip="'RGB(193, 185, 139)'"></div>        
+                <div class="boxes" v-tooltip="'RGB(227, 232, 228)'"></div>
               </div>
               <div class="pallet-color-dark">
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>
-                <div class="boxes"></div>       
-                <div class="boxes"></div>        
-                <div class="boxes"></div>
+                <div class="boxes" v-tooltip="'RGB(114, 100, 162)'"></div>
+                <div class="boxes" v-tooltip="'RGB(0, 159, 214)'"></div>
+                <div class="boxes" v-tooltip="'RGB(0, 158, 78)'"></div>
+                <div class="boxes" v-tooltip="'RGB(250, 223, 34)'"></div>
+                <div class="boxes" v-tooltip="'RGB(214, 69, 76)'"></div>
+                <div class="boxes" v-tooltip="'RGB(212, 27, 98)'"></div>
+                <div class="boxes" v-tooltip="'RGB(240, 247 ,252)'"></div>       
+                <div class="boxes" v-tooltip="'RGB(110, 90, 85)'"></div>        
+                <div class="boxes" v-tooltip="'RGB(52, 51, 54)'"></div>
               </div>
             </div>    
           </div>
@@ -98,26 +98,26 @@
               </div>
             </div>
               <div data-theme="black" class="light-to-dark">
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
-                  <div class="boxes"></div>
+                  <div class="boxes" v-tooltip="'RGB(243, 243, 243)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(217, 217, 217)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(194, 194, 194)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(178, 178, 178)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(157, 157, 157)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(142, 142, 142)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(128, 128, 128)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(115, 115, 115)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(104, 104, 104)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(93, 93, 93)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(84, 84, 84)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(75, 75, 75)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(68, 68, 68)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(61, 61, 61)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(55, 55, 55)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(50, 50, 50)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(45, 45, 45)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(41, 41, 41)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(37, 37, 37)'"></div>
+                  <div class="boxes" v-tooltip="'RGB(34, 34, 34)'"></div>
             </div>
           </div>
         </div>
@@ -125,7 +125,7 @@
         <div v-for="(object, key) in colorData" class="col-md-2" style="text-align: center;">
           {{ key }}
          <!--  <div v-for="(color, index) in object" v-if="index % Math.floor(object.length / 10) === 0 && index < Math.floor(object.length / 10) * 10" class="color-cell" v-bind:style="{ 'background-color': 'rgb(' + color['Red'] + ',' + color['Green'] +',' + color['Blue'] + ')'}" v-on:click="selectColor(key, color)" :title="color['Red'] + ',' + color['Green'] +',' + color['Blue']"> -->
-        <div v-for="(color, index) in object" class="color-cell"  v-if="color['show'] == 1" v-bind:style="{ 'background-color': 'rgb(' + color['Red'] + ',' + color['Green'] +',' + color['Blue'] + ')'}" v-on:click="selectColor(key, color)" v-tooltip="color['Red'] + ', ' + color['Green'] +', ' + color['Blue']">
+        <div v-for="(color, index) in object" class="color-cell"  v-if="color['show'] == 1" v-bind:style="{ 'background-color': 'rgb(' + color['Red'] + ',' + color['Green'] +',' + color['Blue'] + ')'}" v-on:click="selectColor(key, color)" v-tooltip="'RGB('+color['Red'] + ', ' + color['Green'] +', ' + color['Blue']+')'">
           </div>
         </div>
       </div>
